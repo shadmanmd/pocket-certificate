476dfcb3b94a9a91d8a02c998d2ca4a43a165168
Md Shadman <44136375+shadmanmd@users.noreply.github.com>
2021-05-17 16:28:05 +0530

    Create landing page

--- project/app.py
+++ project/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "hello world from anukul"
+def index():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__=='__main__':
 	app.run(debug=True)
